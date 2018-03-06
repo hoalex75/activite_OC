300e27d8afc92e804c10064efe9975fd7279725f
hoalex <svt75@hotmail.com>
2018-03-06 17:10:05 +0100

    Finalisation de la méthode main pour donner le résultat escompté

--- Scierie.java
+++ Scierie.java
@@ -2,6 +2,8 @@ public class Scierie{
 	public static void main(String[] args){
 		Buche buche= new Buche(300,100);
 		Bois tas= buche.coupe(33);
+		
+		System.out.println("Le bois obtenu est de longueur: "+tas.getLongueur()+"cm et possède "+tas.getNombre()+" bouts");
 	}
 
 }
